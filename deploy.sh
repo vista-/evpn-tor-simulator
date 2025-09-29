aa6628e1e6acccd7d8ec4e7037af41adefef1142
vista <vista@birb.network>
2025-09-29 12:19:21 +0100

    Add executable flag to deploy script, disable CGO

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ ip link add bridge2 type bridge
 ip link set dev bridge1 up
 ip link set dev bridge2 up
 
-go build .
+CGO_ENABLED=0 go build .
 
 ## NOTE: MUST USE containerlab BUILT FROM BRANCH feature/idiv-template UNTIL CONTAINERLAB 0.70.3 IS RELEASED
 clab deploy
