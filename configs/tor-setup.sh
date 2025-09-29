4b42abf08f6a9758442d21fc0c356927be1d70b5
vista <vista@birb.network>
2025-09-29 09:29:22 +0100

    Add RRs and spine indexes as variables for ToR nodes

--- configs/tor-setup.sh
+++ configs/tor-setup.sh
@@ -2,18 +2,27 @@
 
 ID=$(hostname | cut -d p -f2)
 
-IPINDEX=$(( ID-1 ))
-NEIGH_IPADDR=$(( IPINDEX*2 ))
-IPADDR=$(( NEIGH_IPADDR + 1 ))
+IPINDEX=$(( ID - 1 ))
+NEIGH_IPADDR=$(( IPINDEX * 2 ))
+LOCAL_IPADDR=$(( NEIGH_IPADDR + 1 ))
+
+RRs=${RRs:-"1.1.1.1,2.2.2.2"}
+RR_ARRAY=($(echo "$RRs" | tr ',' '\n'))
+
+SPINEINDICES=${SPINEINDICES:-"11,12"}
+SPINEINDEX_ARRAY=($(echo "$SPINEINDICES" | tr ',' '\n'))
 
 ip link set dev eth1 up
 ip link set dev eth2 up
 ip link add link eth1 name eth1.${ID} type vlan id ${ID}
 ip link add link eth2 name eth2.${ID} type vlan id ${ID}
-ip addr add 10.11.$((IPADDR/256)).$((IPADDR%256))/31 dev eth1.${ID}
-ip addr add 10.12.$((IPADDR/256)).$((IPADDR%256))/31 dev eth2.${ID}
+for i in $(seq 0 1); do
+  ip addr add 10.${SPINEINDEX_ARRAY[$((i))]}.$((LOCAL_IPADDR / 256)).$((LOCAL_IPADDR % 256))/31 dev eth$((i + 1)).${ID}
+done
 ip link set dev eth1.${ID} up
 ip link set dev eth2.${ID} up
 ip addr add 10.0.$((ID/256)).$((ID%256))/32 dev lo
-ip ro add 1.1.1.1/32 via 10.11.0.${NEIGH_IPADDR}
-ip ro add 2.2.2.2/32 via 10.12.0.${NEIGH_IPADDR}
+
+for j in $(seq 0 1); do
+  ip ro add ${RR_ARRAY[${j}]}/32 via 10.${SPINEINDEX_ARRAY[${j}]}.$((NEIGH_IPADDR / 256)).$((NEIGH_IPADDR % 256))
+done
--- configs/tor-simulator.sh
+++ configs/tor-simulator.sh
@@ -4,11 +4,15 @@ ID=$(hostname | cut -d p -f2)
 
 IPINDEX=$(( ID-1 ))
 NEIGH_IPADDR=$(( IPINDEX*2 ))
-IPADDR=$(( NEIGH_IPADDR + 1 ))
+LOCAL_IPADDR=$(( NEIGH_IPADDR + 1 ))
+
+SPINEINDICES=${SPINEINDICES:-"11,12"}
+LOCAL_ADDRS=$(IFS=,; for i in $SPINEINDICES; do echo -n "10.${i}.$((LOCAL_IPADDR / 256)).$((LOCAL_IPADDR % 256)),"; done | sed 's/,$//')
+NEIGH_ADDRS=$(IFS=,; for j in $SPINEINDICES; do echo -n "10.${j}.$((NEIGH_IPADDR / 256)).$((NEIGH_IPADDR % 256)),"; done | sed 's/,$//')
 
 # Wait a bit for SR Linux nodes to start...
 sleep 10
 
-/evpn-tor-simulator --localaddrs 10.11.$((IPADDR / 256)).$((IPADDR % 256)),10.12.$((IPADDR / 256)).$((IPADDR % 256)) \
-                    --neighbors 10.11.$((NEIGH_IPADDR / 256)).$((NEIGH_IPADDR % 256)),10.12.$((NEIGH_IPADDR / 256)).$((NEIGH_IPADDR % 256)) \
-                    --neighbor-as ${NEIGH_AS} --id ${ID} --bridge-domains ${BD} --macs-per-bd ${MAC}
+/evpn-tor-simulator --localaddrs ${LOCAL_ADDRS} --neighbors ${NEIGH_ADDRS}  --neighbor-as ${NEIGH_AS} \
+                    --rrs ${RRs:-1.1.1.1,2.2.2.2} --rr-as ${RR_AS:-65500} \
+                    --id ${ID} --bridge-domains ${BD} --macs-per-bd ${MAC}
